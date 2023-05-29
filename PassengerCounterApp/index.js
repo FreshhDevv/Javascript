286424f62862d48bcabd30e1d268f664d3b74697
Billy Hans <billyhans90@gmail.com>
2023-05-29 23:57:25 +0100

    Reassingning and incrementing variable values

--- PassengerCounterApp/index.js
+++ PassengerCounterApp/index.js
@@ -1,13 +1,23 @@
 // document.getElementById("count_el").innerText = 5
 
-// let firstBatch = 5;
-// let secondBatch = 7
-// let count = firstBatch + secondBatch;
-// console.log(count);
+
+// let count = 5;
+
+// // count + 1
+// count = count + 1;
+
+//  console.log(count)
 
 
-let myAge = 23;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
-console.log(myDogAge);
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
   
