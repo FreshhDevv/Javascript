831ee1fb02e7a6e9e2fd93a8795584aa9c8e9cee
Billy Hans <billyhans90@gmail.com>
2023-05-30 07:45:26 +0100

    The onclick event

--- PassengerCounterApp/index.js
+++ PassengerCounterApp/index.js
@@ -9,15 +9,8 @@
 //  console.log(count)
 
 
-let bonusPoints = 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+function increment() {
+    
+    console.log("Button was clicked")
+}
   
