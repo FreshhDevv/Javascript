ce9e01f6a331dea0001094fa021d0c3d22a02315
Billy Hans <billyhans90@gmail.com>
2023-05-31 19:03:25 +0100

    I can now keep track of the state of the game

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,13 +1,16 @@
 let firstCard = 11;
 let secondCard = 10;
 let sum = firstCard + secondCard;
+let hasBlackJack = false
 
 
 if(sum <= 20) {
     console.log("Do you want to draw a new card?")
 } else if(sum === 21) {
     console.log("Wohoo! You've got Blackjack!")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game")
 }
 
+console.log(hasBlackJack)
