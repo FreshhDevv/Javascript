357831d478e0e48693d06443b2568dd6f602d61d
Billy Hans <billyhans90@gmail.com>
2023-06-01 07:02:33 +0100

    String variable concatenation

--- PassengerCounterApp/index.js
+++ PassengerCounterApp/index.js
@@ -1,10 +1,17 @@
-let count = 0
-let countEl = document.getElementById("count_el")
-function addCount() {
-    count = count + 1
-    countEl.innerText = count
-}
+// let count = 0
+// let countEl = document.getElementById("count_el")
+// function addCount() {
+//     count = count + 1
+//     countEl.innerText = count
+// }
 
-function save() {
-    console.log(count)
-}
+// function save() {
+//     console.log(count)
+// }
+
+let username = "per"
+
+let message = "You have three new notifications"
+
+let messageUser = message + " " + username
+console.log(messageUser)
