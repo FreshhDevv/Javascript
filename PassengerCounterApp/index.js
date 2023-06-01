999eb571706b94a28cb02ee9669d283e373c0a69
Billy Hans <billyhans90@gmail.com>
2023-06-01 06:41:10 +0100

    Button now increments the count when clicked

--- PassengerCounterApp/index.js
+++ PassengerCounterApp/index.js
@@ -1,52 +1,6 @@
-// document.getElementById("count_el").innerText = 5
-
-
-// let count = 5;
-
-// // count + 1
-// count = count + 1;
-
-//  console.log(count)
-
-
-function increment() {
-
-    console.log("Button was clicked")
+let count = 0
+function addCount() {
+    count = count + 1
+    console.log(count)
+    document.getElementById("count_el").innerText = count
 }
-  
-function countdown() {
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
-}
-
-countdown()
-
-function number() {
-    console.log(42)
-}
-
-number()
-
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
-function sum() {
-    let add = lap1 + lap2 + lap3
-    console.log(add)
-}
-
-sum()
-
-let lapsCompleted = 0
-
-function lapIncrement() {
-    lapsCompleted = lapsCompleted + 1
-}
-lapIncrement()
-lapIncrement()
-lapIncrement()
-
-console.log(lapsCompleted)
