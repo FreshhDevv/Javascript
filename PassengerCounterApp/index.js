3c989207992b8afdd67d13bc648da35e7f543ea9
Billy Hans <billyhans90@gmail.com>
2023-06-01 18:09:06 +0100

    Counter app is now completed

--- PassengerCounterApp/index.js
+++ PassengerCounterApp/index.js
@@ -1,6 +1,7 @@
 let saveEl = document.getElementById("save-el")
 let count = 0
 let countEl = document.getElementById("count_el")
+
 function addCount() {
     count += 1
     countEl.textContent = count
@@ -9,7 +10,8 @@ function addCount() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
-    console.log(saveEl.innerText + count)
+    count = 0
+    countEl.textContent = count
 }
 
 
