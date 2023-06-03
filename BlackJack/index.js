cedd48b5dcad737cc69eda1842b4e79b7dcb2b88
Billy Hans <billyhans90@gmail.com>
2023-06-03 18:40:33 +0100

    If else challenge completed

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,13 +1,20 @@
-let firstCard = 11;
-let secondCard = 10;
-let sum = firstCard + secondCard;
-console.log(sum);
+// let firstCard = 11;
+// let secondCard = 10;
+// let sum = firstCard + secondCard;
+// console.log(sum);
 
 
-if(sum < 21) {
-    console.log("Do you want to draw a new card?")
-} else if(sum === 21) {
-    console.log("Wohoo! You've got Blackjack!")
-} else {
-    console.log("You're out of the game")
+// if(sum < 21) {
+//     console.log("Do you want to draw a new card?")
+// } else if(sum === 21) {
+//     console.log("Wohoo! You've got Blackjack!")
+// } else {
+//     console.log("You're out of the game")
+// }
+
+let age = 12
+if(age < 21) {
+    console.log("You cannot enter the club")
+} else{
+    console.log("Welcome!")
 }
