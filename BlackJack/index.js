cfb5d591184243474f6e0e690017e9d04e7af743
Billy Hans <billyhans90@gmail.com>
2023-06-03 19:20:06 +0100

    Store the logged out message to a variable

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,27 +1,19 @@
-console.log(4 === 3)    //false
-console.log(5 > 2)  //true
-console.log(12 > 2) //true
-console.log(3 < 0)  //false
-console.log(3 >= 3) //true
-console.log(11 <= 11)   //true
-console.log(3 <= 2) //false
+let firstCard = 11;
+let secondCard = 10;
+let sum = firstCard + secondCard 
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 
-// let firstCard = 11;
-// let secondCard = 10;
-// let sum = firstCard + secondCard + 4;
-// let hasBlackJack = false
-// let isAlive = true
+if(sum <= 20) {
+    message = "Do you want to draw a new card?"
+} else if(sum === 21) {
+    message = "Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+} else {
+    isAlive = false
+    message = "You're out of the game"
+}
 
-
-// if(sum <= 20) {
-//     console.log("Do you want to draw a new card?")
-// } else if(sum === 21) {
-//     console.log("Wohoo! You've got Blackjack!")
-//     hasBlackJack = true
-// } else {
-//     isAlive = false
-//     console.log("You're out of the game")
-// }
-
-// console.log(isAlive)
+console.log(message)
