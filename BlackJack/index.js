ebc02a95da8ca62135f21f7db05dd7650ae061cc
Billy Hans <billyhans90@gmail.com>
2023-06-03 18:49:46 +0100

    The if else conditionals in the blackjack game

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,29 +1,13 @@
-// let firstCard = 11;
-// let secondCard = 10;
-// let sum = firstCard + secondCard;
-// console.log(sum);
+let firstCard = 11;
+let secondCard = 10;
+let sum = firstCard + secondCard;
 
 
-// if(sum < 21) {
-//     console.log("Do you want to draw a new card?")
-// } else if(sum === 21) {
-//     console.log("Wohoo! You've got Blackjack!")
-// } else {
-//     console.log("You're out of the game")
-// }
-
-// let age = 12
-// if(age < 21) {
-//     console.log("You cannot enter the club")
-// } else{
-//     console.log("Welcome!")
-// }
-
-let age = 110
-if (age <= 99) {
-    console.log("Not eligible")
-} else if(age === 100) {
-    console.log("Here is your birthday card from the king!")
+if(sum <= 20) {
+    console.log("Do you want to draw a new card?")
+} else if(sum === 21) {
+    console.log("Wohoo! You've got Blackjack!")
 } else {
-    console.log("Not eligible, you have already gotten one")
+    console.log("You're out of the game")
 }
+
