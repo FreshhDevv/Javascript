25c57785544c9c54bb38dea2bcca7a2210b69e3b
Billy Hans <billyhans90@gmail.com>
2023-06-03 19:15:56 +0100

    Boolean practice

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,18 +1,27 @@
-let firstCard = 11;
-let secondCard = 10;
-let sum = firstCard + secondCard + 4;
-let hasBlackJack = false
-let isAlive = true
+console.log(4 === 3)    //false
+console.log(5 > 2)  //true
+console.log(12 > 2) //true
+console.log(3 < 0)  //false
+console.log(3 >= 3) //true
+console.log(11 <= 11)   //true
+console.log(3 <= 2) //false
 
 
-if(sum <= 20) {
-    console.log("Do you want to draw a new card?")
-} else if(sum === 21) {
-    console.log("Wohoo! You've got Blackjack!")
-    hasBlackJack = true
-} else {
-    isAlive = false
-    console.log("You're out of the game")
-}
+// let firstCard = 11;
+// let secondCard = 10;
+// let sum = firstCard + secondCard + 4;
+// let hasBlackJack = false
+// let isAlive = true
 
-console.log(isAlive)
+
+// if(sum <= 20) {
+//     console.log("Do you want to draw a new card?")
+// } else if(sum === 21) {
+//     console.log("Wohoo! You've got Blackjack!")
+//     hasBlackJack = true
+// } else {
+//     isAlive = false
+//     console.log("You're out of the game")
+// }
+
+// console.log(isAlive)
