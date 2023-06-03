7c18249a7e3546144f2dc0ac1c988a6be7a88dd7
Billy Hans <billyhans90@gmail.com>
2023-06-03 18:36:31 +0100

    If else conditionals

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,6 +1,13 @@
-let firstCard = 3
-let secondCard = 10
+let firstCard = 11;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+console.log(sum);
 
-let sum = firstCard + secondCard
 
-console.log(sum)
+if(sum < 21) {
+    console.log("Do you want to draw a new card?")
+} else if(sum === 21) {
+    console.log("Wohoo! You've got Blackjack!")
+} else {
+    console.log("You're out of the game")
+}
