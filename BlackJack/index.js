9b780591013cd36dbf4c9a318a4b20ecb1f6cbe5
Billy Hans <billyhans90@gmail.com>
2023-06-06 03:29:59 +0100

    Refactoring

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -11,18 +11,18 @@ let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
 
-    cardsEl.textContent += firstCard + " " + secondCard;
+    cardsEl.textContent += " " + firstCard + " " + secondCard;
     if(sum <= 20) {
         message = "Do you want to draw a new card?"
-        sumEl.textContent += sum
+        sumEl.textContent += " "+ sum
     } else if(sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
-        sumEl.textContent += sum;
+        sumEl.textContent += " " + sum;
     } else {
         isAlive = false
         message = "You're out of the game!"
-        sumEl.textContent += sum;
+        sumEl.textContent += " " + sum;
     }
     messageEl.textContent = message
 }
