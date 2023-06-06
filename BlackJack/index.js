167bcdba5f6f090a197298199c5409d55f785afb
Billy Hans <billyhans90@gmail.com>
2023-06-06 02:55:38 +0100

    Added the start game button and styled it

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -5,15 +5,18 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+function startGame() {
 
-if(sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if(sum === 21) {
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    isAlive = false
-    message = "You're out of the game"
+    if(sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if(sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        isAlive = false
+        message = "You're out of the game"
+    }
+    
+    console.log(message)
 }
 
-console.log(message)
