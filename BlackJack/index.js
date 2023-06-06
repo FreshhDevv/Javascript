78ae2582c09e1d45ba8c328a6254d0baa604dff5
Billy Hans <billyhans90@gmail.com>
2023-06-06 03:04:45 +0100

    Display the message to the screen and some refactoring

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -4,19 +4,19 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
 
 function startGame() {
 
     if(sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if(sum === 21) {
-        message = "Wohoo! You've got Blackjack!"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
         isAlive = false
-        message = "You're out of the game"
+        message = "You're out of the game!"
     }
-    
-    console.log(message)
+    messageEl.textContent = message
 }
 
