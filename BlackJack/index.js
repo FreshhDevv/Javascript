f0fd0d4fbec209b2703dff9b49769eab94a48f4b
Billy Hans <billyhans90@gmail.com>
2023-06-06 03:15:08 +0100

    Displaying the sum

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -5,17 +5,22 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// let sumEl = document.querySelector("#sum-el")
 
 function startGame() {
 
     if(sum <= 20) {
         message = "Do you want to draw a new card?"
+        sumEl.textContent += sum
     } else if(sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        sumEl.textContent += sum;
     } else {
         isAlive = false
         message = "You're out of the game!"
+        sumEl.textContent += sum;
     }
     messageEl.textContent = message
 }
