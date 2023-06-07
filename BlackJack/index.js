e538d72af383c90a36bf7a9899c0b68aeb139624
Billy Hans <billyhans90@gmail.com>
2023-06-07 22:10:22 +0100

    Intro into arrays

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,5 +1,6 @@
 let firstCard = 4;
 let secondCard = 10;
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
@@ -14,7 +15,7 @@ function startGame() {
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
     if(sum <= 20) {
         message = "Do you want to draw a new card?"
         sumEl.textContent = "Sum: " + sum
