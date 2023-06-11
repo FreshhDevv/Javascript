21dc72d2eaf9196dbdf92fe98eb2d595a51ea4c7
Billy Hans <billyhans90@gmail.com>
2023-06-11 10:28:33 +0100

    For loop with arrays

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -39,10 +39,23 @@
 // }
 
 
-for (let count = 10; count < 21; count += 1) {
-    console.log(count)
-}
+// for (let count = 10; count < 21; count += 1) {
+//     console.log(count)
+// }
 
-for (let i = 10; i < 101; i += 10) {
-    console.log(i)
+// for (let i = 10; i < 101; i += 10) {
+//     console.log(i)
+// }
+
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+    "Great!!!",
+    "Thank you!"
+]
+
+for (let i = 0; i < messages.length; i += 1) {
+    console.log(messages[i])
 }
