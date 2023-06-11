8ca55c119ab69419f98d85b09184ec0fee375867
Billy Hans <billyhans90@gmail.com>
2023-06-11 10:01:26 +0100

    Counting in js

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,41 +1,44 @@
-let firstCard = 4;
-let secondCard = 10;
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard 
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+// let firstCard = 4;
+// let secondCard = 10;
+// let cards = [firstCard, secondCard]
+// let sum = firstCard + secondCard 
+// let hasBlackJack = false
+// let isAlive = true
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+// let cardsEl = document.getElementById("cards-el")
 
-function startGame() {
-    renderGame()
+// function startGame() {
+//     renderGame()
+// }
+
+// function renderGame() {
+//     cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+//     if(sum <= 20) {
+//         message = "Do you want to draw a new card?"
+//         sumEl.textContent = "Sum: " + sum
+//     } else if(sum === 21) {
+//         message = "You've got Blackjack!"
+//         hasBlackJack = true
+//         sumEl.textContent = "Sum: " + sum;
+//     } else {
+//         isAlive = false
+//         message = "You're out of the game!"
+//         sumEl.textContent = "Sum: " + sum;
+//     }
+//     messageEl.textContent = message
+// }
+
+// function newCard() {
+//     console.log("Drawing out new card from the deck")
+//     let thirdCard = 5
+//     sum += thirdCard
+//     cards.push(thirdCard)
+//     renderGame()
+// }
+
+
+for (let count = 10; count < 21; count += 1) {
+    console.log(count)
 }
-
-function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
-    if(sum <= 20) {
-        message = "Do you want to draw a new card?"
-        sumEl.textContent = "Sum: " + sum
-    } else if(sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-        sumEl.textContent = "Sum: " + sum;
-    } else {
-        isAlive = false
-        message = "You're out of the game!"
-        sumEl.textContent = "Sum: " + sum;
-    }
-    messageEl.textContent = message
-}
-
-function newCard() {
-    console.log("Drawing out new card from the deck")
-    let thirdCard = 5
-    sum += thirdCard
-    cards.push(thirdCard)
-    renderGame()
-}
-
-
