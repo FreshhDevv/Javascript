4c965b97067868836854da63e1ae73a1d8544437
Billy Hans <billyhans90@gmail.com>
2023-06-12 12:46:24 +0100

    Start game bug fixed

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard 
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -11,10 +9,22 @@ let cardsEl = document.getElementById("cards-el")
 
 
 function getRandomCard() {
-    return Math.floor(Math.random() * 13) + 1; 
+    let randomCard = Math.floor(Math.random() * 13) + 1
+    if(randomCard === 1) {
+        return 11
+    } else if(randomCard > 10) {
+        return 10
+    } else {
+        return randomCard 
+    }
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard() 
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -39,7 +49,6 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log("Drawing out new card from the deck")
     let thirdCard = getRandomCard();
     sum += thirdCard
     cards.push(thirdCard)
