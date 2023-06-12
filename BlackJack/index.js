90c75b6215bcebfc67852b5d73a24def8a0bcc9a
Billy Hans <billyhans90@gmail.com>
2023-06-12 12:31:05 +0100

    Function that generates random card numbers from 1-13 using the map.random and map.floor functions

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,64 +1,47 @@
-// let firstCard = getRandomCard()
-// let secondCard = getRandomCard();
-// let cards = [firstCard, secondCard]
-// let sum = firstCard + secondCard 
-// let hasBlackJack = false
-// let isAlive = true
-// let message = ""
-// let messageEl = document.getElementById("message-el")
-// let sumEl = document.getElementById("sum-el")
-// let cardsEl = document.getElementById("cards-el")
-
-// function startGame() {
-//     renderGame()
-// }
-
-// function renderGame() {
-//     cardsEl.textContent = "Cards: "
-//     for (let i = 0; i < cards.length; i++) {
-//         cardsEl.textContent += cards[i] + " "
-//     }
-//     if(sum <= 20) {
-//         message = "Do you want to draw a new card?"
-//         sumEl.textContent = "Sum: " + sum
-//     } else if(sum === 21) {
-//         message = "You've got Blackjack!"
-//         hasBlackJack = true
-//         sumEl.textContent = "Sum: " + sum;
-//     } else {
-//         isAlive = false
-//         message = "You're out of the game!"
-//         sumEl.textContent = "Sum: " + sum;
-//     }
-//     messageEl.textContent = message
-// }
-
-// function newCard() {
-//     console.log("Drawing out new card from the deck")
-//     let thirdCard = getRandomCard();
-//     sum += thirdCard
-//     cards.push(thirdCard)
-//     renderGame()
-// }
+let firstCard = getRandomCard()
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard 
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 
+function getRandomCard() {
+    return Math.floor(Math.random() * 13) + 1; 
+}
 
-let player1Time = 102
-let player2Time = 107
+function startGame() {
+    renderGame()
+}
 
-function getFastestRaceTime() {
-    if(player1Time < player2Time) {
-        return player1Time
-    } else if (player2Time < player1Time) {
-        return player2Time
-    } else {
-        return player1Time
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
     }
+    if(sum <= 20) {
+        message = "Do you want to draw a new card?"
+        sumEl.textContent = "Sum: " + sum
+    } else if(sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+        sumEl.textContent = "Sum: " + sum;
+    } else {
+        isAlive = false
+        message = "You're out of the game!"
+        sumEl.textContent = "Sum: " + sum;
+    }
+    messageEl.textContent = message
 }
 
-function totalRaceTime() {
-    return player1Time + player2Time
+function newCard() {
+    console.log("Drawing out new card from the deck")
+    let thirdCard = getRandomCard();
+    sum += thirdCard
+    cards.push(thirdCard)
+    renderGame()
 }
-
-let totalTime = totalRaceTime()
-console.log(totalTime)
