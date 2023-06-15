5021d1fea15f0052961e3de5bd002d813f49f792
Billy Hans <billyhans90@gmail.com>
2023-06-15 08:18:27 +0100

    Moving on to the next section

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,70 +1,72 @@
-// /** @format */
+let player = {
+	name: "Hans",
+	chips: 200,
+};
 
-// let player = {
-// 	name: "Hans",
-// 	chips: 200,
-// };
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-// let cards = [];
-// let sum = 0;
-// let hasBlackJack = false;
-// let isAlive = false;
-// let message = "";
-// let messageEl = document.getElementById("message-el");
-// let sumEl = document.getElementById("sum-el");
-// let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
-// let playerEl = document.getElementById("player-el");
-// playerEl.textContent = player.name + ": $" + player.chips;
+function getRandomCard() {
+	let randomCard = Math.floor(Math.random() * 13) + 1;
+	if (randomCard === 1) {
+		return 11;
+	} else if (randomCard > 10) {
+		return 10;
+	} else {
+		return randomCard;
+	}
+}
 
-// function getRandomCard() {
-// 	let randomCard = Math.floor(Math.random() * 13) + 1;
-// 	if (randomCard === 1) {
-// 		return 11;
-// 	} else if (randomCard > 10) {
-// 		return 10;
-// 	} else {
-// 		return randomCard;
-// 	}
-// }
+function startGame() {
+	isAlive = true;
+	let firstCard = getRandomCard();
+	let secondCard = getRandomCard();
+	cards = [firstCard, secondCard];
+	sum = firstCard + secondCard;
+	renderGame();
+}
 
-// function startGame() {
-// 	isAlive = true;
-// 	let firstCard = getRandomCard();
-// 	let secondCard = getRandomCard();
-// 	cards = [firstCard, secondCard];
-// 	sum = firstCard + secondCard;
-// 	renderGame();
-// }
+function renderGame() {
+	cardsEl.textContent = "Cards: ";
+	for (let i = 0; i < cards.length; i++) {
+		cardsEl.textContent += cards[i] + " ";
+	}
+	if (sum <= 20) {
+		message = "Do you want to draw a new card?";
+		sumEl.textContent = "Sum: " + sum;
+	} else if (sum === 21) {
+		message = "You've got Blackjack!";
+		hasBlackJack = true;
+		sumEl.textContent = "Sum: " + sum;
+	} else {
+		isAlive = false;
+		message = "You're out of the game!";
+		sumEl.textContent = "Sum: " + sum;
+	}
+	messageEl.textContent = message;
+}
 
-// function renderGame() {
-// 	cardsEl.textContent = "Cards: ";
-// 	for (let i = 0; i < cards.length; i++) {
-// 		cardsEl.textContent += cards[i] + " ";
-// 	}
-// 	if (sum <= 20) {
-// 		message = "Do you want to draw a new card?";
-// 		sumEl.textContent = "Sum: " + sum;
-// 	} else if (sum === 21) {
-// 		message = "You've got Blackjack!";
-// 		hasBlackJack = true;
-// 		sumEl.textContent = "Sum: " + sum;
-// 	} else {
-// 		isAlive = false;
-// 		message = "You're out of the game!";
-// 		sumEl.textContent = "Sum: " + sum;
-// 	}
-// 	messageEl.textContent = message;
-// }
+function newCard() {
+	if (isAlive === true && hasBlackJack === false) {
+		let card = getRandomCard();
+		sum += card;
+		cards.push(card);
+		renderGame();
+	}
+}
 
-// function newCard() {
-// 	if (isAlive === true && hasBlackJack === false) {
-// 		let card = getRandomCard();
-// 		sum += card;
-// 		cards.push(card);
-// 		renderGame();
-// 	}
-// }
+
+
+// PRACTICE CHALLENGES
 
 // let person = {
 //     name: "Hans",
@@ -114,12 +116,12 @@
 //     console.log("Spooky face")
 // }
 
-let hands = ["rock", "paper", "scissor"]
+// let hands = ["rock", "paper", "scissor"]
 
-function random()
-{
-    let randomNumber = Math.floor(Math.random() * hands.length)
-    console.log(hands[randomNumber])
-}
+// function random()
+// {
+//     let randomNumber = Math.floor(Math.random() * hands.length)
+//     console.log(hands[randomNumber])
+// }
 
-random()
+// random()
