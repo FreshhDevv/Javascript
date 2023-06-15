0348617cebd9fbdc2662b3235110a1944024af73
Billy Hans <billyhans90@gmail.com>
2023-06-15 07:13:50 +0100

    Practice: push, pop, unshift, shift

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -92,9 +92,17 @@
 //     console.log("senior citizen")
 // }
 
-let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"]
+// let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"]
 
-console.log("The 5 largest countries in the world")
-for(let i = 0; i < largeCountries.length; i++) {
-    console.log("- " + largeCountries[i])
-}
+// console.log("The 5 largest countries in the world")
+// for(let i = 0; i < largeCountries.length; i++) {
+//     console.log("- " + largeCountries[i])
+// }
+
+let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"]
+largeCountries.pop()
+largeCountries.push("Pakistan")
+largeCountries.shift()
+largeCountries.unshift("China")
+
+console.log("- " + largeCountries);
