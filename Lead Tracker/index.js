721421a2d2bcf3d338c16bba330afcbe35bca76b
Billy Hans <billyhans90@gmail.com>
2023-06-30 10:59:07 +0100

    Add the leads and input in the js

--- Lead Tracker/index.js
+++ Lead Tracker/index.js
@@ -1,3 +1,6 @@
+let myLeads = []
+let inputEl = document.getElementById("input-el")
+
 let saveButton = document.getElementById("input-btn")
 saveButton.addEventListener("click", function() {
     console.log("Button Clicked")
